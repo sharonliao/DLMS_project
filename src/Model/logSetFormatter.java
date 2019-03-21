39090802dec9e7560f07336c916279cd523d644c
huiyi999 <chenhuiyi940830@gmail.com>
2019-03-21 13:36:06 -0400

    add logsetformatter

---
+++ src/Model/logSetFormatter.java
@@ -0,0 +1,17 @@
+package Model;
+
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class logSetFormatter extends Formatter{
+
+	@Override
+	public String format(LogRecord record) {
+		// TODO Auto-generated method stub
+		return "[" + new Date() + "]" + " [" + record.getLevel() + "] " + record + "\n";
+	}
+
+	
+
+}
