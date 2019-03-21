8cb4642061461e27410fb4d1922708994de1a8e4
huiyi999 <chenhuiyi940830@gmail.com>
2019-03-21 12:18:23 -0400

    add sequencer

--- src/Model/ResultMsg.java
+++ src/Model/ResultMsg.java
@@ -4,54 +4,51 @@ package Model;
  * Created by liaoxiaoyun on 2019-03-12.
  */
 public class ResultMsg {
-    /**
-     * Add
-     */
-    static String Ad0 = "Successfully add.";
-    static String Ad1 = "Unsuccessfully add.";
-    static String Ad2 = "Automatically lend the item to the user in waitlist.";
+	/**
+	 * Add
+	 */
+	public static String Ad0 = "Successfully add.";
+	public static String Ad1 = "Unsuccessfully add.";
+	public static String Ad2 = "Automatically lend the item to the user in waitlist.";
 
+	/**
+	 * Remove
+	 */
+	public static String Re0 = "Successfully remove.";
+	public static String Re1 = "Successfully decrease.";
+	public static String Re2 = "Unsuccessfully remove.";
+	public static String Re3 = "Such item does not exist.";
+	public static String Re4 = "Invalid operation. Please try again.";
 
-    /**
-     *Remove
-     */
-    static String Re0 = "Successfully remove.";
-    static String Re1 = "Successfully decrease.";
-    static String Re2 = "Unsuccessfully remove.";
-    static String Re3 = "Such item does not exist.";
-    static String Re4 = "Invalid operation. Please try again.";
+	/**
+	 * borrow result
+	 */
 
+	public static String Br0 = "Successfully borrow.";
+	public static String Br1 = "Unsuccessfully borrow.";
+	public static String Br2 = "Such item does not exist.";
+	public static String Br3 = "This item is unavailable, do you want to add to waiting list?";
+	public static String Br4 = "You have borrowed this item.";
+	public static String Br5 = "You have borrowed from this library.";
+	public static String Br6 = "You are already in the waitlist.";
 
-    /**
-     * borrow result
-     */
-
-    static String	Br0 = "Successfully borrow.";
-    static String	Br1 = "Unsuccessfully borrow.";
-    static String	Br2 = "Such item does not exist.";
-    static String	Br3 = "This item is unavailable, do you want to add to waiting list?";
-    static String	Br4 = "You have borrowed this item.";
-    static String	Br5 = "You have borrowed from this library.";
-    static String	Br6 = "You are already in the waitlist.";
-
-    /**
-     * return
-     */
-    static String	Rtn0 = "Successfully return.";
-    static String	Rtn1 = "Unsuccessfully return.";
-    static String	Rtn2 = "Such item does not exist.";
-    static String	Rtn3 = "Automatically lend the item to the user in waitlist.";
-
-
-    /**
-     * exchange
-     */
-    static String	Ex0 = "Successfully exchange.";
-    static String	Ex1 = "Successfully exchange.";
-    static String	Ex2 = "Such item does not exist.";
-    static String	Ex3 = "This item is unavailable, do you want to add to waiting list?";
-    static String	Ex4 = "You have borrowed this item.";
-    static String	Ex5 = "You have borrowed from this library.";
-    static String	Ex6 = "You are already in the waitlist.";
-    static String	Ex7 = "You didn't borrow this item.";
+	/**
+	 * return
+	 */
+	public static String Rtn0 = "Successfully return.";
+	public static String Rtn1 = "Unsuccessfully return.";
+	public static String Rtn2 = "Such item does not exist.";
+	public static String Rtn3 = "Automatically lend the item to the user in waitlist.";
+	public static String Rtn4 = "You didn't borrow this item.";
+	/**
+	 * exchange
+	 */
+	public static String Ex0 = "Successfully exchange.";
+	public static String Ex1 = "Successfully exchange.";
+	public static String Ex2 = "Such item does not exist.";
+	public static String Ex3 = "This item is unavailable, do you want to add to waiting list?";
+	public static String Ex4 = "You have borrowed this item.";
+	public static String Ex5 = "You have borrowed from this library.";
+	public static String Ex6 = "You are already in the waitlist.";
+	public static String Ex7 = "You didn't borrow this item.";
 }
